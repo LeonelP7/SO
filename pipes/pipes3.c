a55d4c7a827bef873ce3f8816eb410600569befc
Leonel <pereiraleonel287@gmail.com>
2025-04-30 16:30:49 -0500

    tercer ejercicio de pipes

--- pipes/pipes3.c
+++ pipes/pipes3.c
@@ -36,19 +36,100 @@ int main(int argc, char const *argv[]) {
   }
 
   if (nProceso == 2) {
-    do
-    {
-        printf("Write 'fin' to exit.\n");
-        fgets(buff,MAX_READ,stdin);
-        if (strlen(buff) > 1)
-        {
-            buff[strlen(buff) - 1] = EOL; 
-        }
-        
-        
+    do {
 
-    } while (condition);
-    
+      for (int i = 0; i < 2; i++) {
+
+        close(fd1[i][0][0]);
+        close(fd1[i][1][1]);
+      }
+
+      printf("Write 'fin' to exit.\n");
+      fgets(buff, MAX_READ, stdin);
+      if (strlen(buff) > 1) {
+        buff[strlen(buff) - 1] = EOL;
+        if (strcmp(buff, "fin") != 0) {
+
+          write(fd1[0][0][1], buff, MAX_READ);
+          write(fd1[1][0][1], buff, MAX_READ);
+          printf("[%d]writes --> %s\n", getpid(), buff);
+
+          for (int i = 0; i < 2; i++) {
+            if ((nRead = read(fd1[i][1][0], buff, MAX_READ)) > 0) {
+              buff[nRead] = EOL;
+              printf("[%d]reads <-- %s\n", getpid(), buff);
+            }
+          }
+        }
+      }
+    } while (strcmp(buff, "fin") != 0);
+
+    for (int i = 0; i < 2; i++) {
+
+      close(fd1[i][0][1]);
+      close(fd1[i][1][0]);
+    }
+    for (int i = 0; i < 2; i++) {
+      wait(NULL);
+    }
+  } else {
+    if (nProceso < 2) {
+
+      for (int i = 0; i < 2; i++) {
+        if (i == nProceso) {
+          close(fd1[i][1][0]);
+          close(fd1[i][0][1]);
+        } else {
+          for (int j = 0; j < 2; j++) {
+            close(fd1[i][j][0]);
+            close(fd1[i][j][1]);
+          }
+        }
+      }
+
+      close(fd1[2][0][0]);
+      close(fd1[2][1][1]);
+
+      while ((nRead = read(fd1[nProceso][0][0], buff, MAX_READ))>0) {
+        buff[nRead] = EOL;
+        printf("[%d][%d]reads <-- %s\n", getpid(), getppid(), buff);
+        write(fd1[2][0][1], buff, MAX_READ);
+        printf("[%d][%d]write --> %s\n", getpid(), getppid(), buff);
+
+        if ((nRead = read(fd1[2][1][0], buff, MAX_READ))>0) {
+          buff[nRead] = EOL;
+          printf("[%d][%d]reads <-- %s\n", getpid(), getppid(), buff);
+          write(fd1[nProceso][1][1], buff, MAX_READ);
+          printf("[%d][%d]write --> %s\n", getpid(), getppid(), buff);
+        }
+      }
+
+      close(fd1[2][0][1]);
+      close(fd1[2][1][0]);
+      wait(NULL);
+
+    } else {
+
+      for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+          close(fd1[i][j][0]);
+          close(fd1[i][j][1]);
+        }
+      }
+
+      close(fd1[2][0][1]);
+      close(fd1[2][1][0]);
+
+      while ((nRead = read(fd1[2][0][0], buff, MAX_READ))>0) {
+        buff[nRead] = EOL;
+        printf("[%d]reads <-- %s\n", getpid(), buff);
+        write(fd1[2][1][1], buff, MAX_READ);
+        printf("[%d]write --> %s\n", getpid(), buff);
+      }
+
+      close(fd1[2][0][0]);
+      close(fd1[2][1][1]);
+    }
   }
 
 
