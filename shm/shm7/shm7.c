512f38feaadfba0c6bd2b0f610e918cb9eb015a8
Leonel <pereiraleonel287@gmail.com>
2025-05-12 22:48:07 -0500

    detalle shm7

--- shm/shm7/shm7.c
+++ shm/shm7/shm7.c
@@ -87,6 +87,7 @@ int main(int argc, char const *argv[]) {
       while (contador < hijos) {
         pause();
       }
+      contador = 0;
 
       for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
